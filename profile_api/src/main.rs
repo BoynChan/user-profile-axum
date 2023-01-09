2b35d3c66dfa7acc4712855afb63945857706f62
chenzhanpeng.boynn <chenzhanpeng.boynn@bytedance.com>
2023-01-09 17:49:03 +0800

    feat: add store layer for users

--- profile_api/src/main.rs
+++ profile_api/src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello Api!");
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref USERS: Arc<Mutex<Vec<User>>> = Arc::new(Mutex::new(Vec::new()));
+}
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user))
+        .route("/users", get(list_user));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> impl IntoResponse {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+    USERS.clone().lock().unwrap().push(user.clone());
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+async fn list_user() -> impl IntoResponse {
+    let u:Vec<User> = USERS.clone().lock().unwrap().to_vec();
+    (StatusCode::OK, Json(u))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize, Clone)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize, Clone)]
+struct User {
+    id: u64,
+    username: String,
 }
